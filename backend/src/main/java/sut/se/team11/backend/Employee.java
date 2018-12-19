8474c48a6c04d36e24c32920a9a7c84ed89f3350
saridap <saridam10@gmail.com>
2018-12-19 13:40:05 +0700

    close #15 - make entity starter ของระบบย่อย บันทึกข้อมูลพนักงาน

---
+++ backend/src/main/java/sut/se/team11/backend/Employee.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Employee {
+
+	private @Id @GeneratedValue Long id;
+	
+
+	private Employee() {}
+
+	
+}
