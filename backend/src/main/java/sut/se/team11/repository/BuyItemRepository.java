9bbae888d33bdd8f5814b084cad556c285df5773
MHides <memories.cpem@gmail.com>
2019-01-22 20:39:05 +0700

    close #51 - add all repository

---
+++ backend/src/main/java/sut/se/team11/repository/BuyItemRepository.java
@@ -0,0 +1,9 @@
+package sut.se.team11.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import sut.se.team11.entity.BuyItem;
+
+@RepositoryRestResource
+public interface BuyItemRepository extends JpaRepository<BuyItem, Long> {
+}
---
+++ backend/src/main/java/sut/se/team11/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package sut.se.team11.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import sut.se.team11.entity.Cart;
+
+@RepositoryRestResource
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
