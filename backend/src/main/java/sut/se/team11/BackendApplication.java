e02c1bb5621d78edd7b9c4e025fc6a960ae23e8a
Midnight01 <patarawit.miv@gmail.com>
2019-01-22 20:21:56 +0700

    ทำ การเตรียมข้อมูลใน BackendApplication ในส่วนของ entity ที่จะต้องมีข้อมูล เพื่อในการกรองข้อมูลลูกค้า - close #49

--- backend/src/main/java/sut/se/team11/BackendApplication.java
+++ backend/src/main/java/sut/se/team11/BackendApplication.java
@@ -1,7 +1,12 @@
 package sut.se.team11;
 
+import sut.se.team11.repository.*;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sut.se.team11.entity.*;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,6 +14,36 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	@Bean
+	ApplicationRunner init(CustomerRepository customerRepository, BranchRepository branchRepository, CareerRepository careerRepository,
+						   ProvinceRepository provinceRepository
+	){
+		return  args -> {
 
+			Stream.of("สาขา 1","สาขา 2","สาขา 3").forEach(bName -> {
+
+				Branch branch = new Branch();
+				branch.setBName(bName);
+				branchRepository.save(branch);
+
+			});
+
+			Stream.of("ข้าราชการ/รัฐวิสาหกิจ","ธุรกิจส่วนตัว/ค้าขาย","เกษตรกร","รับจ้าง/พนักงานบริษัทเอกชน","นักเรียน/นักศึกษา","อื่น ๆ").forEach(cName -> {
+
+				Career career = new Career();
+				career.setCName(cName);
+				careerRepository.save(career);
+
+			});
+			Stream.of("กาฬสินธุ์","ขอนแก่น","ชัยภูมิ","นครพนม","นครราชสีมา","บึงกาฬ","บุรีรัมย์","มหาสารคาม","มุกดาหาร","ยโสธร","ร้อยเอ็ด","เลย","ศรีสะเกษ","สกลนคร","สุรินทร์","หนองคาย","หนองบัวลำภู","อุดรธานี","อุบลราชธานี","อำนาจเจริญ").forEach(pName -> {
+
+
+				Province province = new Province();
+				province.setPName(pName);
+				provinceRepository.save(province);
+
+			});
+
+		};
+	}
 }
-
