f88a839306f45b267d04dcec463b0e32509ca682
MHides <memories.cpem@gmail.com>
2019-01-24 00:12:47 +0700

    close #64 - add UnitController and CategoryController

---
+++ backend/src/main/java/sut/se/team11/controller/CategoryController.java
@@ -0,0 +1,20 @@
+package sut.se.team11.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import sut.se.team11.entity.Category;
+import sut.se.team11.repository.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class CategoryController {
+    @Autowired private CategoryRepository categoryRepository;
+
+    @GetMapping(path = "/getCategories")
+    private List<Category> getCategories(){
+        return categoryRepository.findAll().stream().collect(Collectors.toList());
+    }
+}
