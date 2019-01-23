fdb49836ae797e042ea3679ab0074945fd18d8b7
parinya <parinya78@hotmail.com>
2019-01-23 22:13:17 +0700

    close #60 - Make SaleItem Entity

---
+++ backend/src/main/java/sut/se/team11/entity/SaleItem.java
@@ -0,0 +1,39 @@
+package sut.se.team11.entity;
+
+import lombok.*;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Data
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class SaleItem {
+    @Id
+    @SequenceGenerator(name = "sale_seq", sequenceName = "sale_seq")
+    @GeneratedValue(generator = "sale_seq", strategy = GenerationType.SEQUENCE)
+    private Long saleItemId;
+    private @NonNull String itemName;
+    private @NonNull double price;
+    private @NonNull int amount;
+    private @NonNull double totalPrice;
+    private @NonNull Date date;
+
+
+    @ManyToOne
+    @JoinColumn(name = "customerId")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "employeeId")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "buyItemId")
+    private BuyItem buyItem;
+
+}
