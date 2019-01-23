82e83cc6046fbf66ab62d171b8af34e6b5b0a707
MHides <memories.cpem@gmail.com>
2019-01-23 22:02:03 +0700

    close #59 - fix Error

--- backend/src/main/java/sut/se/team11/entity/BuyItem.java
+++ backend/src/main/java/sut/se/team11/entity/BuyItem.java
@@ -1,5 +1,6 @@
 package sut.se.team11.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -21,6 +22,7 @@ public class BuyItem {
 
     @ManyToOne
     @JoinColumn(name = "cartId")
+    @JsonIgnore
     private Cart cart;
     @ManyToOne
     @JoinColumn(name = "categoryId")
