053d4497353dac0e76c95b841f2e0dbe27187408
MHides <memories.cpem@gmail.com>
2019-01-25 01:26:24 +0700

    close #80 - add model class

---
+++ client/src/app/shared/models/model-class.ts
@@ -0,0 +1,30 @@
+export class Cart{
+    cartId: any;
+    paymentStatus: string;
+    date: Date;
+    customer: Customer;
+}
+export class BuyItem{
+    buyItemId: any;
+    itemName: string;
+    price: any;
+    amount: number;
+    totalPrice: any;
+    cart: Cart;
+    category: Category;
+    unit: Unit;
+}
+export class Category{
+    categoryId: any;
+    categoryName: string;
+}
+export class Unit{
+    unitId: any;
+    unitName: string;
+}
+export class Customer{
+    customerId: any;
+    customerName: string;
+    address: string;
+    tel: any;
+}
