af253a77975cfdc7999ebe0fedc7f1d94eedb72b
b5815159 <saridam10@gmail.com>
2019-01-25 22:48:00 +0700

    add employee service - close #92

--- client/src/app/components/employee/employee.component.ts
+++ client/src/app/components/employee/employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/shared/employee/employee.service';
+import { HttpClient} from '@angular/common/http';
+import { EmployeeService } from 'src/app/shared/employee/employee.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  employee : Employee = new Employee();
+  branchs : Array<any>;
+  positions : Array<any>;
+  educations : Array<any>;
+  province : Array<any>;
 
-  ngOnInit() {
+  constructor(private service : EmployeeService , private httpClient: HttpClient
+    ) { }
+
+    ngOnInit() {
+      this.service.getBranch().subscribe(data => {
+        this.branchs = data;
+      });
+      this.service.getEducation().subscribe(data => {
+        this.educations = data;
+      });
+      this.service.getPosition().subscribe(data => {
+        this.positions = data;
+      });
+      this.service.getProvince().subscribe(data => {
+        this.province = data;
+      });
+    }
+  save(){
+    console.log(this.employee.bdate);
+    this.httpClient.post('//localhost:8080/Employee/' + this.employee.title + '/'+
+    this.employee.eName + '/' + this.employee.age + '/' + this.employee.bdate + '/' + 
+    this.employee.tel + '/' + this.employee.address + '/'+ this.employee.province.pid + '/' + 
+    this.employee.education.edid + '/' + this.employee.branch.bid + '/' + this.employee.position.psid
+    ,this.employee).subscribe(
+      data => {
+        console.log('Save Successful, Congratulations ', data);
+      },
+      error => {
+        console.log('!!!! Warning, Error !!!!!', error);
+      }
+       
+    );
   }
 
 }  
---
+++ client/src/app/shared/employee/employee.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+  };
+  public APT = '//localhost:8080';
+
+  constructor(private http: HttpClient) { }
+  getEducation(): Observable<any> {
+    return this.http.get(this.APT + '/Education');
+     
+  }
+
+  getPosition(): Observable<any> {
+    return this.http.get(this.APT + '/Position');
+   
+  }
+
+  getBranch(): Observable<any> {
+    return this.http.get(this.APT + '/Branch');
+   
+  }
+
+  getProvince(): Observable<any> {
+    return this.http.get(this.APT + '/Province');
+   
+  }
+}
+export class Branch {
+  bid:any;
+  bname: String;
+}
+export class Position {
+  psid:any;
+  psname:String;
+}
+export class Education {
+  edid: any;
+  edname : String;
+}
+export class Province { 
+  pid : any;
+  pname : String;
+}
+export class Employee {
+  eId : any;
+  title : any ;
+  eName : any;
+  bdate : any;
+  age : any;
+  tel :any ;
+  address : any;
+  province : Province;
+  education : Education;
+  branch : Branch;
+  position : Position;
+
+
+}
+
+
+
