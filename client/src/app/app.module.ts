25a202319b994876e21df06ca47d94127bf42fee
b5815159 <saridam10@gmail.com>
2019-01-25 20:11:52 +0700

    ทำ UI ของระบบย่อยบันทึกข้อมูลพนักงาน - close #89

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,36 +21,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PageLayoutComponent } from './page-layout/page-layout.component';
 import { CustomerComponent } from './components/customer/customer.component';
-<<<<<<< HEAD
-import { FindCustomerComponent } from './components/find-customer/find-customer.component';
-=======
 import { EmployeeComponent } from './components/employee/employee.component';  
->>>>>>> ทำ employee UI - close #89
 
 const routes: Routes = [
   {path: '', redirectTo: '/customer', pathMatch: 'full'},
   {path: 'customer', component: CustomerComponent},
-<<<<<<< HEAD
-  {path: 'find-customer', component: FindCustomerComponent}
-=======
-  {path: 'employee', component: EmployeeComponent}  
->>>>>>> ทำ employee UI - close #89
+  {path: 'employee', component: EmployeeComponent} 
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     PageLayoutComponent,
-
-    
     CustomerComponent,
-
-    
-<<<<<<< HEAD
-    FindCustomerComponent
-=======
     EmployeeComponent
->>>>>>> ทำ employee UI - close #89
   ],
   imports: [
     BrowserModule,
