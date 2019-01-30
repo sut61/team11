17e7f16d427d64a7b929016d6dc545072faabfcc
Mormook <Kultida_55@hotmail.com>
2019-01-30 22:18:44 +0700

    ทำการทำ servive เชื่อมกับ backend -close #103

--- client/src/app/shared/quotation/quotation.service.spec.ts
+++ client/src/app/shared/quotation/quotation.service.spec.ts
@@ -5,6 +5,8 @@ import { QuotationService } from './quotation.service';
 describe('QuotationService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
+
+
   it('should be created', () => {
     const service: QuotationService = TestBed.get(QuotationService);
     expect(service).toBeTruthy();
