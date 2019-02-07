42e4a1a1bfe1369cae948cdff96d0b24cd8e3efe
Midnight01 <patarawit.miv@gmail.com>
2019-02-07 22:54:33 +0700

    ทำ หน้า UI ตรวจสอบสภาพสินค้า - close #139

--- client/src/app/components/check/check.component.ts
+++ client/src/app/components/check/check.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CheckService, Checks } from 'src/app/shared/check/check.service';
+import { CheckEmployeeService,Employee } from 'src/app/shared/checkEmployee/check-employee.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckComponent implements OnInit {
 
-  constructor() { }
+  checks: Checks = new Checks();
+  id2: any;
+  employee: Employee = new Employee();
+  sub2: Subscription;
+  date: Date;
+  BuyItems: Array<any>;
+  Categorizes: Array<any>;
+  Check1s: Array<any>;
+  constructor(private service:CheckService,
+    private find2: CheckEmployeeService,
+    // private route: Router,
+    private httpClient: HttpClient,
+    private route: ActivatedRoute,
+     ) {
+      this.date = new Date();
+      }
 
   ngOnInit() {
-  }
+    this.service.getBuyItems().subscribe(data => {
+      this.BuyItems = data;
+      console.log(this.BuyItems);
+    });
+    this.service.getCategorize().subscribe(data => {
+      this.Categorizes = data;
+    });
+    this.service.getChecks().subscribe(data => {
+        this.Check1s = data;
+        console.log(this.Check1s);
+      });
+  this.sub2 = this.route.params.subscribe(param => {
+    this.id2 = param['id'];
+    // console.log(this.id);
+    this.find2.checkEmployee(this.id2).subscribe((data) => {
+      this.employee = data;
+    });
+  }, err => {
+    console.log(err + "Errororororor");
+  });
+}
 
 }
