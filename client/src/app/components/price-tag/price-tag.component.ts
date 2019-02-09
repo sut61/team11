58d09005d51ec75d6021c4c72c67469f3c691f41
b5815159 <saridam10@gmail.com>
2019-02-10 06:55:28 +0700

    ทำหน้า UI ของระบบและแก้ไขฐานข้อมูล - close #199

--- client/src/app/components/price-tag/price-tag.component.ts
+++ client/src/app/components/price-tag/price-tag.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PriceTagService, SavePrice } from '../../shared/priceTag/price-tag.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-price-tag',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceTagComponent implements OnInit {
 
-  constructor() { }
+  
+  priceTagId : any;
+  saveprices : Array<SavePrice>
+  categories : Array<any>;
+  usedItems : Array<any>;
+  employees : Array<any>;
+
+  //displayedColumns: string[] = ['priceTagId','','categoryOne','priceOne','categoryTwo','priceTwo','usedItemId','employeeId'];
+  
+  constructor(private router: Router,
+    private service : PriceTagService) { }
 
   ngOnInit() {
+    // this.showPriceTag();
+    this.service.SavePrice().subscribe(data => {
+      this. saveprices = data;
+      console.log(this. saveprices);
+    });
+    
+  }
+  saveprice(){
+    this.router.navigate(['/saveprice']).then(() =>{
+
+    });
   }
 
 }
